028065a7afe81c57f8027a7158642d18ffaae615
ankitsng0007 <ankitsng0007@gmail.com>
2023-12-09 17:43:09 +0530

    File added

---
+++ toll_calculator/src/Pages/Redux/tollReducer.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux';
+
+const initialSate = {
+    amount:null,
+    error:null
+};
+const tollReducer = (state=initialSate, action) => {
+    switch(action.type){
+        case 'CALCULATE_TOLL_SUCESS' :
+            return {
+                amount: action.payload,
+                error: null,
+            };
+        case 'CALCULATE_TOLL_FAILURE':
+            return {
+                amount: null,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+const rootReducer = combineReducers({toll:tollReducer})
+
+export default rootReducer;
