035ee3fdb8c20df85b5ab30ae8e97ec7c7192d7c
ankitsng0007 <ankitsng0007@gmail.com>
2023-12-09 17:52:50 +0530

    File added

--- toll_calculator/src/Pages/Redux/store.js
+++ toll_calculator/src/Pages/Redux/store.js
@@ -1,7 +1,9 @@
-import { applyMiddleware, createStore } from "redux";
-import { thunk } from "redux-thunk";
+// store.js
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk'; // Use Redux Thunk for async actions if needed
+import rootReducer from './tollReducer';
 
 
-const store = createStore(applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
